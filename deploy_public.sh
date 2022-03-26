12a897f4ca3e695b928cd984aa9f01e6e5749161
cmtr <46787135+cmtr@users.noreply.github.com>
2022-03-26 11:53:17 +0100

    update deployment file

--- deploy_public.sh
+++ deploy_public.sh
@@ -5,31 +5,95 @@ changed=0
 
 echo `date +"%Y-%m-%d-%T"` - Check for update to public site
 
+## got to project root folder
+cd ..
+
+
+
 # check for updates on cms
 echo `date +"%Y-%m-%d-%T"` - Check for updates on content
 cd cms-public
-#git remote update > /dev/null 2>&1
-#git status -uno | grep -q 'Your branch is behind' && changed=$((changed+1))
+hostChanged=1;
+git remote update > /dev/null 2>&1
+git status -uno | grep -q 'Your branch is behind' && hostChanged=$((hostChanged+1))
+changed=$(($changed+$hostChanged))
+
+if [ "$hostChanged" -gt "0" ]; then
+	echo `date +"%Y-%m-%d %T"` - Something changed on content
+	
+	echo `date +"%Y-%m-%d-%T"` - Updating repository
+	git pull
+
+else
+	echo `date +"%Y-%m-%d %T"` - Nothing changed on content
+fi
 
 cd ..
+
+
 
 # check for updates on host
 echo `date +"%Y-%m-%d-%T"` - Check for updates on host
 cd public
-#git remote update > /dev/null 2>&1
-#git status -uno | grep -q 'Your branch is behind' && changed=$((changed+1))
+hostChanged=1;
+git remote update > /dev/null 2>&1
+git status -uno | grep -q 'Your branch is behind' && hostChanged=$((hostChanged+1))
+changed=$(($changed+$hostChanged))
+
+if [ "$hostChanged" -gt "0" ]; then
+	echo `date +"%Y-%m-%d %T"` - Something changed on host site
+
+	echo `date +"%Y-%m-%d-%T"` - Start Docker Container cmtr-public
+	npm run stop-docker
+
+	echo `date +"%Y-%m-%d-%T"` - Updating repository
+	git pull
+	
+	echo `date +"%Y-%m-%d-%T"` - Building Docker Image cmtr-public
+	npm run build
+	
+	echo `date +"%Y-%m-%d-%T"` - Start Docker Container cmtr-public
+	npm run start-docker
+else
+	echo `date +"%Y-%m-%d %T"` - Nothing changed on host site
+fi
+
 cd ..
+
+
 
 # check for updates on page-crawler
 echo `date +"%Y-%m-%d-%T"` - Check for updates on scraper
 cd page-crawler
-#git remote update > /dev/null 2>&1
-#git status -uno | grep -q 'Your branch is behind' && changed=$((changed+1))
+
+
+hostChanged=1;
+git remote update > /dev/null 2>&1
+git status -uno | grep -q 'Your branch is behind' && hostChanged=$((hostChanged+1))
+changed=$(($changed+$hostChanged))
+
+
+if [ "$hostChanged" -gt "0" ]; then
+	echo `date +"%Y-%m-%d %T"` - Something changed on host site
+
+	echo `date +"%Y-%m-%d-%T"` - Updating repository
+	git pull
+
+	echo `date +"%Y-%m-%d-%T"` - Update dependencies
+	npm install
+
+	echo `date +"%Y-%m-%d-%T"` - Scraping Content
+	npm run start-prod
+
+else
+	echo `date +"%Y-%m-%d %T"` - Nothing changed on scraper
+fi
+
 cd ..
 
-# if [ $changed =< 0 ]; then
-if [ true ]; then
 
+
+if [ "$changed" -gt "0" ]; then
 
 	# Update static file host
 	echo `date +"%Y-%m-%d %T"` - Start update to public site required
@@ -56,7 +120,7 @@ if [ true ]; then
 	fileListFileName="../../logs/`date +"%Y-%m-%d-%T"`-file-list.txt"
 	# touch $fileListFileName
 	echo `date +"%Y-%m-%d %T"` - Upload content
-	cd page-crawler/copy
+	cd page-crawler/output
 	for file in $(find $(pwd) -type f -not -path '*/\.*' | sort)
 	do
 		echo $file ${file#"$(pwd)"} >> $fileListFileName
@@ -93,3 +157,5 @@ if [ true ]; then
 else
 	echo `date +"%Y-%m-%d %T"` - Nothing changed on public site
 fi
+
+echo `date +"%Y-%m-%d-%T"` - Finish update to public site
